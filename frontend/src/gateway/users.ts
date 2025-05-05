5a715fdd57e55114bce92888da8480b228931bae
baoobab <v.xxl.90@mail.ru>
2025-05-05 16:23:52 +0300

    chore: роут для селектора

--- frontend/src/gateway/users.ts
+++ frontend/src/gateway/users.ts
@@ -2,7 +2,7 @@ import { IUser, IUserUpdate } from "types/user";
 import { requestUsers } from "./api";
 
 const getMe = () => requestUsers.get<IUser>('/me');
-const getUsers = () => requestUsers.get<{data: IUser[], count: number}>('');
+const getUsers = () => requestUsers.get<{data: IUser[], count: number}>('/select');
 const patchUser = (userData: IUserUpdate) => requestUsers.patch<IUser>(`/me`, userData);
 
 export default {
